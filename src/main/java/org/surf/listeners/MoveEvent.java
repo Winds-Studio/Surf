598622a74b6f72f857652bd2189dc1143fc69f4f
Dreeam <dreeam_charlie@163.com>
2021-12-20 10:25:55 +0800

    Fix & Bump ver

--- src/main/java/org/surf/listeners/MoveEvent.java
+++ src/main/java/org/surf/listeners/MoveEvent.java
@@ -24,7 +24,7 @@ public class MoveEvent implements Listener {
 			int z = player.getLocation().getBlockZ();
 			Location bottom = new Location(player.getWorld(), z, 5, x);
 			Location top = new Location(player.getWorld(), z, 125, x);
-			if (plugin.getConfig().getString("Nether.Enabled").equalsIgnoreCase("false")
+			if (plugin.getConfig().getString("Nether.Enabled").equalsIgnoreCase("true")
 					&& !(player.hasPermission("surf.netherroof.bypass"))) {
 
 				if (player.getWorld().getEnvironment() == Environment.NETHER
