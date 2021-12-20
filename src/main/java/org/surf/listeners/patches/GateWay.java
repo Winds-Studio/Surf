377836f43984e4a0c53293b2a7dad847242aac4e
Dreeam <dreeam_charlie@163.com>
2021-12-20 14:34:59 +0800

    Update msg

--- src/main/java/org/surf/listeners/patches/GateWay.java
+++ src/main/java/org/surf/listeners/patches/GateWay.java
@@ -100,7 +100,7 @@ public class GateWay implements Listener {
 							event.getVehicle().eject();
 							event.getVehicle().remove();
 							player.chat(">>IM A FAG WHO JUST TRIED TO CRASH THE SERVER");
-							Utils.kickPlayer(player, "[&b&lL2X9&r&3&lCore&r]&6 Sorry that exploit got patched ):");
+							Utils.kickPlayer(player, "[&b&lSurf&r]&6 Sorry that exploit got patched ):");
 							System.out.println(ChatColor.translateAlternateColorCodes('&', "&1Prevented&r&e " + player.getName() + "&r&1 at &r&e" + x + " " + y + " " + z + " &r&1in world&e " + worldString + " &r&1from crashing the server"));
 						}
 					}
