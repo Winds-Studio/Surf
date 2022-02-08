f013eda4ed8aa45165ef9746a35d5dfc80662ba5
Dreeam <dreeam_charlie@163.com>
2022-02-08 16:45:33 +0800

    revert InvertoryOpen & Close

---
+++ src/main/java/org/surf/listeners/antiillegal/InventoryClose.java
@@ -0,0 +1,48 @@
+package org.surf.listeners.antiillegal;
+
+//import org.bukkit.block.ShulkerBox;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+//import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+//import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+//import org.bukkit.inventory.meta.BlockStateMeta;
+import org.surf.Main;
+
+public class InventoryClose implements Listener {
+    Main plugin;
+
+    public InventoryClose(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    @AntiIllegal(EventName = "InventoryCloseEvent")
+    public void onInventoryClose(InventoryCloseEvent event) {
+        try {
+            if (plugin.getConfig().getBoolean("Antiillegal.InventoryClose-Enabled")) {
+                Inventory inv = event.getInventory();
+                plugin.getItemUtils().deleteIllegals((PlayerInventory) inv);
+                Inventory playerInv = event.getPlayer().getInventory();
+                plugin.getItemUtils().deleteIllegals((PlayerInventory) playerInv);
+//                if (event.getInventory().getType() == InventoryType.SHULKER_BOX) {
+//                    Inventory shulkerInv = event.getInventory();
+//                    for (ItemStack item : shulkerInv.getContents()) {
+//                        if (item != null) {
+//                            if (item.getItemMeta() instanceof BlockStateMeta) {
+//                                BlockStateMeta blockStateMeta = (BlockStateMeta) item.getItemMeta();
+//                                if (blockStateMeta.getBlockState() instanceof ShulkerBox) {
+//                                    shulkerInv.remove(item);
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+            }
+        } catch (Error | Exception throwable) {
+
+        }
+    }
+}
---
+++ src/main/java/org/surf/listeners/antiillegal/InventoryOpen.java
@@ -0,0 +1,29 @@
+package org.surf.listeners.antiillegal;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.PlayerInventory;
+import org.surf.Main;
+
+public class InventoryOpen implements Listener {
+    Main plugin;
+
+    public InventoryOpen(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    @AntiIllegal(EventName = "InventoryOpenEvent")
+    public void onInventoryClose(InventoryOpenEvent event) {
+        try {
+            if (plugin.getConfig().getBoolean("Antiillegal.InventoryOpen-Enabled")) {
+                Inventory inv = event.getInventory();
+                plugin.getItemUtils().deleteIllegals((PlayerInventory) inv);
+            }
+        } catch (Error | Exception throwable) {
+
+        }
+    }
+}
