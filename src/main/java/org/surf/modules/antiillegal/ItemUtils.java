b9b638a3220ee839a2bb0abde5cb960eac9b80ec
Loyisa <loyisa893@gmail.com>
2022-11-11 21:14:59 +0800

    Improve deleteIllegals

--- src/main/java/org/surf/modules/antiillegal/ItemUtils.java
+++ src/main/java/org/surf/modules/antiillegal/ItemUtils.java
@@ -78,36 +78,42 @@ public class ItemUtils {
     }
 
     public static void deleteIllegals(Inventory inventory) {
-        try {
-            ItemStack itemStack = null;
-            ItemMeta itemMeta = null;
-            boolean illegalsFound = false;
-            if (inventory.getContents() != null) {
-                for (ItemStack item : inventory.getContents()) {
-                    if (item != null) {
-                            if (item.getDurability() > item.getType().getMaxDurability()) {
-                                item.setDurability(item.getType().getMaxDurability());
-                                itemStack = item;
-                            }
-                            if (item.getDurability() < 0) {
-                                item.setDurability((short) 1);
-                                itemStack = item;
-                            }
+        // TODO: use a list to store the items to delete
+        ItemStack itemStack = null;
+        boolean illegalsFound = false;
+        // if inventory is empty, skip
+        if (inventory.getContents().length == 0) {
+            return;
+        }
+        for (ItemStack item : inventory.getContents()) {
+            // if item is null, skip
+            if (item == null) {
+                continue;
+            }
+            if (item.getDurability() > item.getType().getMaxDurability()) {
+                item.setDurability(item.getType().getMaxDurability());
+                itemStack = item;
+            }
+            if (item.getDurability() < 0) {
+                item.setDurability((short) 1);
+                itemStack = item;
+            }
 
-                        if (isIllegal(item)) {
-                            inventory.remove(item);
-                            illegalsFound = true;
-                            itemStack = item;
-                        }
-                        if (hasIllegalItemFlag(item)) {
-                            inventory.remove(item);
-                            illegalsFound = true;
-                            itemStack = item;
-
-                        }
-                        if (hasIllegalAttributes(item)) {
-                            inventory.remove(item);
-                            //TODO
+            if (isIllegal(item)) {
+                inventory.remove(item);
+                illegalsFound = true;
+                itemStack = item;
+                continue;
+            }
+            if (hasIllegalItemFlag(item)) {
+                inventory.remove(item);
+                illegalsFound = true;
+                itemStack = item;
+                continue;
+            }
+            if (hasIllegalAttributes(item)) {
+                inventory.remove(item);
+                //TODO
 //                            item.getItemMeta().removeAttributeModifier(Attribute.GENERIC_ARMOR_TOUGHNESS);
 //                            item.getItemMeta().removeAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE);
 //                            item.getItemMeta().removeAttributeModifier(Attribute.GENERIC_ATTACK_KNOCKBACK);
@@ -120,35 +126,31 @@ public class ItemUtils {
 //                            item.getItemMeta().removeAttributeModifier(Attribute.GENERIC_MOVEMENT_SPEED);
 //                            item.getItemMeta().removeAttributeModifier(Attribute.HORSE_JUMP_STRENGTH);
 //                            item.getItemMeta().removeAttributeModifier(Attribute.ZOMBIE_SPAWN_REINFORCEMENTS);
-                            illegalsFound = true;
-                            itemStack = item;
-                        }
-                        if (hasIllegalEnchants(item)) {
-                            for (Entry<Enchantment, Integer> enchantmentIntegerEntry : item.getEnchantments().entrySet()) {
-                                item.removeEnchantment(enchantmentIntegerEntry.getKey());
-                                illegalsFound = true;
-                                itemStack = item;
-                            }
-                        }
-                        if (item.hasItemMeta()) {
-                            if (isEnchantedBlock(item)) {
-                                Iterator<Entry<Enchantment, Integer>> enchants = item.getEnchantments().entrySet()
-                                        .iterator();
-                                illegalsFound = true;
-                                itemStack = item;
-                                while (enchants.hasNext()) {
-                                    item.removeEnchantment(enchants.next().getKey());
-                                }
-                            }
-                        }
+                illegalsFound = true;
+                itemStack = item;
+                continue;
+            }
+            if (hasIllegalEnchants(item)) {
+                for (Entry<Enchantment, Integer> enchantmentIntegerEntry : item.getEnchantments().entrySet()) {
+                    item.removeEnchantment(enchantmentIntegerEntry.getKey());
+                    illegalsFound = true;
+                    itemStack = item;
+                }
+            }
+            if (item.hasItemMeta()) {
+                if (isEnchantedBlock(item)) {
+                    Iterator<Entry<Enchantment, Integer>> enchants = item.getEnchantments().entrySet()
+                            .iterator();
+                    illegalsFound = true;
+                    itemStack = item;
+                    while (enchants.hasNext()) {
+                        item.removeEnchantment(enchants.next().getKey());
                     }
                 }
             }
-            if (illegalsFound) {
-                Utils.println(Utils.getPrefix() + "&6Deleted illegals " + itemStack.getType() + " " + itemStack.getI18NDisplayName() + " " + itemStack.getEnchantments() + " " + itemMeta.getAttributeModifiers());
-            }
-        } catch (Error | Exception throwable) {
-
+        }
+        if (illegalsFound) {
+            Utils.println(Utils.getPrefix() + "&6Deleted illegals " + itemStack.getType() + " " + itemStack.getI18NDisplayName() + " " + itemStack.getEnchantments() + (itemStack.hasItemMeta() ? " " + itemStack.getItemMeta().getAttributeModifiers() : ""));
         }
     }
 
