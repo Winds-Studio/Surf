d93c944b36e912393755f3a40a44a4660706df01
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-03-24 18:12:45 -0400

    Fix stacked totem bypass

--- src/main/java/cn/dreeam/surf/modules/antiillegal/StackedTotem.java
+++ src/main/java/cn/dreeam/surf/modules/antiillegal/StackedTotem.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class StackedTotem implements Listener {
 
@@ -17,10 +16,6 @@ public class StackedTotem implements Listener {
         // Check when player gets damage or make damage to others
         if (event.getEntity() instanceof Player) {
             Player entity = (Player) event.getEntity();
-            ItemStack item = entity.getInventory().getItemInMainHand();
-            ItemStack item2 = entity.getInventory().getItemInOffHand();
-
-            if (!ItemUtil.isIllegalTotem(item) && !ItemUtil.isIllegalTotem(item2)) return;
 
             entity.getInventory().forEach(i -> {
                         if (i != null && ItemUtil.isIllegalTotem(i)) {
