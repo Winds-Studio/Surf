677c211bf5021afa933187d6ccf108f0dea0fe3a
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-07-25 17:28:29 +0800

    Remove pearl add back to prevent dupe

--- src/main/java/cn/dreeam/surf/modules/patch/PreventEnderPearlClip.java
+++ src/main/java/cn/dreeam/surf/modules/patch/PreventEnderPearlClip.java
@@ -1,13 +1,10 @@
 package cn.dreeam.surf.modules.patch;
 
 import cn.dreeam.surf.config.Config;
-import com.cryptomorin.xseries.XMaterial;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 public class PreventEnderPearlClip implements Listener {
 
@@ -24,17 +21,6 @@ public class PreventEnderPearlClip implements Listener {
         // Prevent teleport to same block location to try stuck in the block
         if (toBlock.getX() == fromBlock.getX() && toBlock.getY() == fromBlock.getY() && toBlock.getZ() == fromBlock.getZ()) {
             event.setCancelled(true);
-
-            /*
-            Add pearl back to inv
-            search for the pearl in the inv, and if player consumes the last pearl in inv,
-            create a new one. This can largely prevent pearl name inconsistency.
-             */
-            Inventory inv = event.getPlayer().getInventory();
-            int slot = inv.first(XMaterial.ENDER_PEARL.parseMaterial());
-            ItemStack pearl = new ItemStack(XMaterial.ENDER_PEARL.parseMaterial(), 1);
-            ItemStack item = slot != -1 ? inv.getItem(slot) : pearl;
-            inv.addItem(item != null ? item : pearl);
         }
     }
 }
