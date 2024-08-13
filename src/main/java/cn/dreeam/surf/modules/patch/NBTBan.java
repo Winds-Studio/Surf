d9f9356ead1d8a2c7964f32fc3ee938c7d0b67f6
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-08-13 22:18:39 +0800

    Fix prevent nbt ban

--- src/main/java/cn/dreeam/surf/modules/patch/NBTBan.java
+++ src/main/java/cn/dreeam/surf/modules/patch/NBTBan.java
@@ -2,46 +2,46 @@ package cn.dreeam.surf.modules.patch;
 
 import cn.dreeam.surf.config.Config;
 import cn.dreeam.surf.util.Util;
-import de.tr7zw.changeme.nbtapi.NBT;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
-
-import java.util.concurrent.atomic.AtomicInteger;
+import org.bukkit.inventory.ItemStack;
 
 public class NBTBan implements Listener {
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         if (!Config.preventNBTBanEnabled) return;
 
         Inventory inv = event.getPlayer().getInventory();
-        AtomicInteger itemSize = new AtomicInteger();
 
-        // TODO
         if (Util.isNewerAndEqual(20, 5)) {
-            inv.forEach(i -> {
+            for (ItemStack i : inv) {
                 if (i != null && i.getType().name().contains("SHULKER_BOX")) {
-                    itemSize.addAndGet(NBT.itemStackToNBT(i).toString().length());
+                    if (!i.hasItemMeta()) continue;
 
-                    if (itemSize.get() > Config.preventNBTBanLimit) {
+                    int itemSize = i.getItemMeta().getAsComponentString().length();
+
+                    if (itemSize > Config.preventNBTBanLimit) {
                         inv.remove(i);
                         Util.sendMessage(event.getPlayer(), Config.preventNBTBanMessage);
                     }
                 }
-            });
+            }
         } else {
-            inv.forEach(i -> {
+            for (ItemStack i : inv) {
                 if (i != null && i.getType().name().contains("SHULKER_BOX")) {
-                    itemSize.addAndGet(NBT.itemStackToNBT(i).toString().length());
+                    if (!i.hasItemMeta()) continue;
 
-                    if (itemSize.get() > Config.preventNBTBanLimit) {
+                    int itemSize = i.getItemMeta().getAsString().length();
+
+                    if (itemSize > Config.preventNBTBanLimit) {
                         inv.remove(i);
                         Util.sendMessage(event.getPlayer(), Config.preventNBTBanMessage);
                     }
                 }
-            });
+            }
         }
     }
 }
