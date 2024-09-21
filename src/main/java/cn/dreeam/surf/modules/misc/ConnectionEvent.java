63a43c1c71478071fee41a1b2cbbcdba716ba789
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-09-21 09:59:31 -0400

    Improve compatibility of connection message

--- src/main/java/cn/dreeam/surf/modules/misc/ConnectionEvent.java
+++ src/main/java/cn/dreeam/surf/modules/misc/ConnectionEvent.java
@@ -1,11 +1,13 @@
 package cn.dreeam.surf.modules.misc;
 
-import cn.dreeam.surf.Surf;
 import cn.dreeam.surf.config.Config;
+import cn.dreeam.surf.util.MessageUtil;
 import cn.dreeam.surf.util.Util;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,22 +22,37 @@ public class ConnectionEvent implements Listener {
         if (!Config.connectionMessageEnabled) return;
 
         Player player = event.getPlayer();
-        Component message = Config.connectionFirstJoinEnabled && !player.hasPlayedBefore()
-                ? getConnectionMessage(player, Config.connectionFirstJoinMessage)
-                : getConnectionMessage(player, Config.connectionPlayerJoin);
 
-        event.setJoinMessage(null);
-        Surf.getInstance().adventure().all().sendMessage(message);
+        if (Util.isNewerAndEqual(16, 0)) {
+            Component message = Config.connectionFirstJoinEnabled && !player.hasPlayedBefore()
+                    ? getConnectionMessageModern(player, Config.connectionFirstJoinMessage)
+                    : getConnectionMessageModern(player, Config.connectionPlayerJoin);
+
+            event.setJoinMessage(null);
+            broadcastConnectionMessage(message);
+        } else {
+            String message = Config.connectionFirstJoinEnabled && !player.hasPlayedBefore()
+                    ? getConnectionMessageLegacy(player, Config.connectionFirstJoinMessage)
+                    : getConnectionMessageLegacy(player, Config.connectionPlayerJoin);
+
+            event.setJoinMessage(message);
+        }
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
         if (!Config.connectionMessageEnabled) return;
 
-        Component message = getConnectionMessage(event.getPlayer(), Config.connectionPlayerLeave);
+        if (Util.isNewerAndEqual(16, 0)) {
+            Component message = getConnectionMessageModern(event.getPlayer(), Config.connectionPlayerLeave);
 
-        event.setQuitMessage(null);
-        Surf.getInstance().adventure().all().sendMessage(message);
+            event.setQuitMessage(null);
+            broadcastConnectionMessage(message);
+        } else {
+            String message = getConnectionMessageLegacy(event.getPlayer(), Config.connectionPlayerLeave);
+
+            event.setQuitMessage(message);
+        }
     }
 
     @EventHandler
@@ -46,11 +63,11 @@ public class ConnectionEvent implements Listener {
 
         if (Config.connectionKickReasons.contains(reason)) {
             event.setCancelled(true);
-            Util.println("Cancelled a kick for " + event.getPlayer().getName() + ", Reason: " + reason);
+            MessageUtil.println("Cancelled a kick for " + event.getPlayer().getName() + ", Reason: " + reason);
         }
     }
 
-    private Component getConnectionMessage(Player player, String message) {
+    private Component getConnectionMessageModern(Player player, String message) {
         Component connecntionComponent;
 
         if (Config.connectionMessageUseDisplayName && !player.displayName().equals(Component.empty())) {
@@ -62,5 +79,26 @@ public class ConnectionEvent implements Listener {
         }
 
         return connecntionComponent;
+    }
+
+    private String getConnectionMessageLegacy(Player player, String message) {
+        String displayName = player.getDisplayName();
+        message = ChatColor.translateAlternateColorCodes('&', message);
+
+        if (Config.connectionMessageUseDisplayName && !displayName.isEmpty()) {
+            message = message.replace("%player%", displayName);
+        } else {
+            message = message.replace("%player%", player.getName());
+        }
+
+        return message;
+    }
+
+    private static void broadcastConnectionMessage(Component message) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player == null) continue;
+
+            player.sendMessage(message);
+        }
     }
 }
